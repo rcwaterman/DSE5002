f3fbb855902f03577e2f364784fb3dda361190b7
Ryan <watermanrw@gmail.com>
2025-04-14 09:13:22 -0400

    Adding homework

---
+++ Module_05/Lab/file_choose.py
@@ -0,0 +1,4 @@
+from tkinter import filedialog
+
+def file_choose():
+    return filedialog.askopenfilename()
---
+++ Module_05/Lab/file_choose_utilization.py
@@ -0,0 +1,4 @@
+from file_choose import file_choose
+
+if __name__ == '__main__':
+    print(file_choose())
