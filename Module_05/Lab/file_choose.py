6b95d249ac296a0b57feedd1b649bf2964edb6bc
rcwaterman <134216651+rcwaterman@users.noreply.github.com>
2025-04-16 19:47:50 -0400

    adding course work so far

--- Module_05/Lab/file_choose.py
+++ Module_05/Lab/file_choose.py
@@ -1,4 +1,6 @@
+# I have always used filedialog from tkinter for this task. Import it...
 from tkinter import filedialog
 
+# Create the file_choose function that returns the response from the askopenfilename function
 def file_choose():
     return filedialog.askopenfilename()
--- Module_05/Lab/file_choose_utilization.py
+++ Module_05/Lab/file_choose_utilization.py
@@ -1,4 +1,6 @@
+# Import the file_choose function
 from file_choose import file_choose
 
+# Run the main function that contains the file_choose function. Print the return.
 if __name__ == '__main__':
     print(file_choose())
