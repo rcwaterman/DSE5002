355160d0c912eaaef5fd6735a4163916f8378f41
Ryan <watermanrw@gmail.com>
2025-04-30 20:39:40 -0500

    cleaning up repo

--- Module_06/Lab/date_14_11_4.py
+++ Module_06/Lab/date_14_11_4.py
@@ -31,17 +31,20 @@ class Date():
         """
         Validate the year input.
 
-        This function will assign the self.year attribute and identify if the year is a leap year.
+        This function will assign the self.year attribute and identify if the 
+        year is a leap year.
 
         Additionally, it will set the self.bc flag if the year is less than 0
         """
 
-        # Per the article, below, the first time a leap year was instituted was 46 B.C.
+        # Per the article, below, the first time a leap year was instituted 
+        # was 46 B.C.
         # https://billpetro.com/history-of-leap-year/
         if int(year) < -46:
             #This is a BC year
             self.bc = True
-            #Get the absolute value of the year. For example, the year will be represented as 500 B.C. instead of -500 
+            #Get the absolute value of the year. For example, the year will be 
+            #represented as 500 B.C. instead of -500 
             self.year=abs(year)
         elif int(year) < 0: 
             self.bc = True
@@ -52,7 +55,8 @@ class Date():
             self.bc = False
             #assign the year
             self.year=year
-            # Check to see if the year is a leap year. This will reassign the number of days in February
+            # Check to see if the year is a leap year. This will reassign the 
+            # number of days in February
             self._is_leap(year)
     
 
@@ -60,7 +64,8 @@ class Date():
         """
         Validate the month based on the rules defined in make_date().
 
-        This function will perform the self.month attribute assignment if the month is valid.
+        This function will perform the self.month attribute assignment if the 
+        month is valid.
         """
         try:
             # Use the month type to determine how to process it...
@@ -68,14 +73,16 @@ class Date():
                 # Error handling for invalid months
                 if month > 12 or month < 1:
                     raise Exception("ERROR: Invalid Month.")
-                # The dictionary will retain it's insertion order, so index into it and get the key where month = index+1
+                # The dictionary will retain it's insertion order, so index 
+                # into it and get the key where month = index+1
                 else:
                     for index, key in enumerate(self.MONTH_MAPPING.keys()):
                         if month == index+1:
                             self.month=key
                             break
 
-            # Raise an exception if the month key does not return a value, otherwise assign it to the month attribute
+            # Raise an exception if the month key does not return a value, 
+            # otherwise assign it to the month attribute
             elif type(month)==str:
                 if not self.MONTH_MAPPING[f"{month}"]:
                     raise Exception("ERROR: Invalid Month.")
@@ -89,7 +96,8 @@ class Date():
         """
         Validate the day input.
 
-        This function will perform the self.day attribute assignment if the day is valid.
+        This function will perform the self.day attribute assignment if the 
+        day is valid.
         """
         try:
             if day > self.MONTH_MAPPING[self.month] or day < 1:
@@ -109,7 +117,8 @@ class Date():
         # No year that is not divisble by 4 can be a leap year
         if year%4:
             self.MONTH_MAPPING["February"] = 28
-        # If the year is divisible by 4, it could still not be a leap year because of the turn of the century
+        # If the year is divisible by 4, it could still not be a leap year 
+        # because of the turn of the century
         elif not year%4:
             # Every 4 centuries will be a leap year 
             if not year%400:
@@ -123,7 +132,8 @@ class Date():
 
     def date_to_int(self) -> int:
         """
-        Convert the current date to an integer that represents the number of days since day 0 for date comparison.
+        Convert the current date to an integer that represents the number of 
+        days since day 0 for date comparison.
         """
         if self.bc:
             year_to_days = (self.year*365)-floor(self.year/4)
@@ -147,13 +157,14 @@ class Date():
 
         Month supports both integer and string inputs. 
         
-        Strings must be the full month name with the first letter capitalized. For example: February
+        Strings must be the full month name with the first letter capitalized. 
+        For example: February
 
         NOTE: 
-        The validation order is critical, and must occur in year, month, day order
-        Both the year and month must be valid before checking for leap year. 
-        If it is a leap year, February will have an additional day, which will be 
-        validated against the self.MONTH_MAPPING dictionary.
+        The validation order is critical, and must occur in year, month, day 
+        order. Both the year and month must be valid before checking for leap 
+        year. If it is a leap year, February will have an additional day, which 
+        will be validated against the self.MONTH_MAPPING dictionary.
 
         """
 
@@ -163,9 +174,10 @@ class Date():
         self.day=day
 
         """ 
-        NOTE: This is me being fancy, I apologize in advance. The validation functions are used 
-        to implement error checking for the respective inputs. Also, this allows for accurate
-        date checking with leap years accounted for.
+        NOTE: This is me being fancy, I apologize in advance. The validation 
+        functions are used to implement error checking for the respective 
+        inputs. Also, this allows for accurate date checking with leap years 
+        accounted for.
         """
 
         # Validate the year input and assign it to the self.year attribute
@@ -174,12 +186,15 @@ class Date():
         # Validate the month input and assign it to the self.month attribute
         self._validate_month(month)
 
-        # Now that the self.MONTH_MAPPING dictionary contains the correct number of days for February, validate the day input and assign the self.day attribute
+        # Now that the self.MONTH_MAPPING dictionary contains the correct 
+        # number of days for February, validate the day input and assign the 
+        # self.day attribute
         self._validate_day(day)
 
     def print_date(self) -> str:
         """
-        Take in a date object and print the date. If the self.bc flag is set, append a BC tag to the date.
+        Take in a date object and print the date. If the self.bc flag is set, 
+        append a BC tag to the date.
         """
         if self.bc:
             print(f"{self.year}-{self.month}-{self.day} BC")
